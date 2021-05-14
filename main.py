e6882b17162d457aa3de61670aba21fad4e1fa65
Rahul <rahulkmishra20@gmail.com>
2021-05-14 11:02:11 +0200

    removed errors

--- main.py
+++ main.py
@@ -12,31 +12,31 @@ def euclidean_distance(row1, row2):
 
 
 # Locate the best matching unit
-def get_best_matching_unit(codebooks, test_row):
+def get_best_matching_unit(prototypes, test_row):
     distances = list()
-    for codebook in codebooks:
+    for codebook in prototypes:
         dist = euclidean_distance(codebook, test_row)
         distances.append((codebook, dist))
     distances.sort(key=lambda tup: tup[1])
     return distances[0][0]
 
 
-# Create a random codebook vector
-def random_codebook(train):
-    n_records = len(train)
-    n_features = len(train[0])
-    codebook = [train[randrange(n_records)][i] for i in range(n_features)]
-    return codebook
+# Create a random prototype vector
+def random_prototype(train_data):
+    n_records = len(train_data)
+    n_features = len(train_data[0])
+    prototype = [train_data[randrange(n_records)][i] for i in range(n_features)]
+    return prototype
 
 
-# Train a set of codebook vectors
-def train_codebooks(train, n_codebooks, lrate, epochs):
-    codebooks = [random_codebook(train) for i in range(n_codebooks)]
+# Train a set of prototype vectors
+def train_codebooks(train, no_of_prototypes, learning_rate, epochs):
+    prototypes = [random_prototype(train) for i in range(no_of_prototypes)]
     for epoch in range(epochs):
-        rate = lrate * (1.0 - (epoch / float(epochs)))
+        rate = learning_rate * (1.0 - (epoch / float(epochs)))
         sum_error = 0.0
         for row in train:
-            bmu = get_best_matching_unit(codebooks, row)
+            bmu = get_best_matching_unit(prototypes, row)
             for i in range(len(row) - 1):
                 error = row[i] - bmu[i]
                 sum_error += error ** 2
@@ -45,7 +45,7 @@ def train_codebooks(train, n_codebooks, lrate, epochs):
                 else:
                     bmu[i] -= rate * error
         print('>epoch=%d, lrate=%.3f, error=%.3f' % (epoch, rate, sum_error))
-    return codebooks
+    return prototypes
 
 
 # Test the training function
